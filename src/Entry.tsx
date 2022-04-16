580fcfada0801162d2dfc22b2ab0c1f72e9257a4
arcrad <jonathanmaiorana@gmail.com>
2022-04-16 01:57:51 -0400

    started implementing entry editor component

--- src/Entry.tsx
+++ src/Entry.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, Dispatch, SetStateAction } from 'react';
+import React, { useState, useEffect, useRef, Dispatch, SetStateAction, ChangeEvent } from 'react';
 import { useLiveQuery } from 'dexie-react-hooks';
 
 import { db, Entry } from './db';
@@ -13,12 +13,37 @@ function EntryComponent({
 	globalState, 
 	setGlobalState
 } : EntryAttributes ) {
-	let storeImageRef = useRef<HTMLButtonElement>(null);
+
+	let [currentEntryWeight, setCurrentEntryWeight] = useState("123");
+	let [currentEntryDate, setCurrentEntryDate] = useState("april 5, 2022");
+	
+let storeImageRef = useRef<HTMLButtonElement>(null);
 	let imageUploadRef = useRef<HTMLInputElement>(null);
-  
+  let entrySelectRef = useRef<HTMLSelectElement>(null);
+
 	const entries = useLiveQuery(
 		() => db.entries.toArray()
 	);
+
+	const currentEntry = useLiveQuery(
+		() => db.entries.get(globalState.currentEntryId)
+	, [globalState]);
+
+	useEffect( () => {
+		let handleEntrySelectChange = (event:Event) => {
+			//console.dir(event);
+			let newEntryId = parseInt( (event.target as HTMLSelectElement).value );
+			//console.log('value=', newEntryId);
+			setGlobalState( (cs):GlobalState => {
+				let ns = { currentEntryId: newEntryId };
+				return { ...cs, ...ns }; 
+			});
+		};
+		entrySelectRef.current?.addEventListener('change', handleEntrySelectChange);
+		return (
+			() => entrySelectRef.current?.removeEventListener('change', handleEntrySelectChange)
+		);
+	}, []);
 
 	useEffect( () => {
 		const addDbEntry = async (imageData:string) => {
@@ -90,7 +115,47 @@ function EntryComponent({
 				console.error(`encountered error trying to delete record with id = ${event.target.dataset.entryId}`);
 			}
 		}
-	}
+	};
+
+	const handleMarkEntry = async (event:React.MouseEvent<HTMLButtonElement>) => {
+		console.log('handleMarkEntry');
+	};
+	
+	const handleChangeImageEntry = async (event:React.MouseEvent<HTMLButtonElement>) => {
+		console.log('handleChangeImageEntry');
+	};
+
+	let debounceInputTimeout = useRef(0);
+	const handleEntryInputChange = async (event:React.ChangeEvent<HTMLInputElement>) => {
+		console.log('handleEntryInputChange');
+		if(
+			event.target
+			&& event.target instanceof HTMLInputElement
+			&& event.target.dataset.entryId
+			&& event.target.dataset.entryKeyToModify
+		) {
+			let entryIdToModify = parseInt(event.target.dataset.entryId);
+			console.log('entryId = ', entryIdToModify);
+			console.log('entryKeyToModify = ', event.target.dataset.entryKeyToModify);
+			console.log('value = ', event.target.value);
+			if(event.target.dataset.entryKeyToModify === 'weight') {
+				setCurrentEntryWeight(event.target.value);
+			} else {
+				setCurrentEntryDate(event.target.value);
+			}
+			clearTimeout(debounceInputTimeout.current);
+			let modifyDbValueHandler = () => {
+					console.log('fire update db with new input');
+					if(event.target.dataset.entryKeyToModify) {
+					db.entries.update(entryIdToModify, {
+						[event.target.dataset.entryKeyToModify]: event.target.value
+					});
+					}
+			};
+
+			debounceInputTimeout.current = window.setTimeout( modifyDbValueHandler, 300);
+		}
+	};
 
 	return (
     <div>
@@ -98,6 +163,55 @@ function EntryComponent({
 			<button ref={storeImageRef} type="button" id="store_image">Store Image</button>
 			<div>
 				<h1>Entries ( id = {globalState.currentEntryId} )</h1>
+				<select ref={entrySelectRef} value={globalState.currentEntryId}>
+				{
+					entries?.map( entry =>
+						<option value={entry.id} key={entry.id}>{entry.id}: {entry.date}</option>
+					)
+				}
+				</select>
+				<hr/>
+							<div>
+								<input 
+									type="text" 
+									value={currentEntryWeight} 
+									data-entry-id={currentEntry?.id} 
+									data-entry-key-to-modify="weight" 
+									onChange={handleEntryInputChange}
+								/> @ 
+								<input 
+									type="text" 
+									value={currentEntryDate}
+									data-entry-id={currentEntry?.id} 
+									data-entry-key-to-modify="date"
+									onChange={handleEntryInputChange}
+								/>
+							</div>
+							<img src={currentEntry?.image} style={{maxWidth: '30rem'}}/>
+							<div>
+								<button 
+									type="button" 
+									data-entry-id={currentEntry?.id} 
+									onClick={handleMarkEntry}
+								>
+									Mark
+								</button>
+								<button 
+									type="button" 
+									data-entry-id={currentEntry?.id} 
+									onClick={handleChangeImageEntry}
+								>
+									Change Image
+								</button>
+								<button 
+									type="button" 
+									data-entry-id={currentEntry?.id} 
+									onClick={handleDeleteEntry}
+								>
+									Delete
+								</button>
+							</div>
+				<hr/>
 				{ 
 					entries?.map( entry => 
 						<div key={entry.id} style={{border: '1px solid black', padding: '1rem', margin: '1rem'}}>
