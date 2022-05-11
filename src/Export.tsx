b0d56720218890d9034d73ab4c8ecb0743dfbf27
arcrad <jonathanmaiorana@gmail.com>
2022-05-11 00:03:00 -0400

    refined export component, added information overlay toggles, refined layout, added placeholders for other export methods, added additional delay between frame rendering updated TODOs

--- src/Export.tsx
+++ src/Export.tsx
@@ -21,27 +21,51 @@ function Export({
 }: ExportAttributes) { 
 	let [statusMessages, setStatusMessages] = useState<string[]>([]);
 	let [frameDuration, setFrameDuration] = useState<number>(150);
+	let [videoIsReady, setVideoIsReady] = useState<boolean>(false);
+	let [overlayFrameNumberIsChecked, setOverlayFrameNumberIsChecked] = useState<boolean>(false);
+	let [overlayEntryInfoIsChecked, setOverlayEntryInfoIsChecked] = useState<boolean>(false);
+	let [entries, setEntries] = useState<Entry[]|null>(null);
 
 	const initializedRef = useRef<boolean>(false);
 	const videoElementRef = useRef<HTMLVideoElement|null>(null);
 	const videoSourceElementRef = useRef<HTMLSourceElement|null>(null);
 	const frameDurationInputRef = useRef<HTMLInputElement|null>(null);
+	const overlayFrameNumberInputRef = useRef<HTMLInputElement|null>(null);
+	const overlayEntryInfoInputRef = useRef<HTMLInputElement|null>(null);
 
 	useEffect( () => {
 		if(initializedRef.current) {
 			return;
 		}
-		console.log('intialize settings from DB started');
+		console.log('intialize data from DB started');
 		initializedRef.current = true;
 		Promise.all([
-			db.settings.get('exportFrameDuration')
+		  db.entries.orderBy('date').toArray(),
+			db.settings.get('exportFrameDuration'),
+			db.settings.get('exportOverlayFrameNumber'),
+			db.settings.get('exportOverlayEntryInfo')
 		]).then( ([
-			_exportFrameDurationSetting
+			_entries,
+			_exportFrameDurationSetting,
+			_exportOverlayFrameNumber,
+			_exportOverlayEntryInfo
 		]) => {
-			console.log('intialize settings from DB finished');
+			console.log('intialize data from DB finished');
+			if(_entries) {
+				console.log(`entries.length = ${_entries.length}`);
+				setEntries(_entries);	
+			}
 			if(_exportFrameDurationSetting) {
 				console.log(`set frameDuration to ${_exportFrameDurationSetting.value}`);
 				setFrameDuration(_exportFrameDurationSetting.value);
+			}
+			if(_exportOverlayFrameNumber) {
+				console.log(`setOverlayFrameNumberIsChecked = ${_exportOverlayFrameNumber}`);
+				setOverlayFrameNumberIsChecked(_exportOverlayFrameNumber.value);
+			}
+			if(_exportOverlayEntryInfo) {
+				console.log(`setOverlayEntryInfoIsChecked = ${_exportOverlayEntryInfo}`);
+				setOverlayEntryInfoIsChecked(_exportOverlayEntryInfo.value);
 			}
 		});
 	}, []);
@@ -60,9 +84,13 @@ function Export({
 
 	const handleExportVideo = async () => {
 		console.log('handleExportVideo() called');
+		if(!entries) {
+			console.error('no entries found');
+			return;
+		}
 		setStatusMessages(["begin generating video for export"]);
 		//let entries = await db.entries.orderBy('date').reverse().toArray();
-		let entries:Entry[] = await db.entries.orderBy('date').toArray();
+		//let entries:Entry[] = await db.entries.orderBy('date').toArray();
 		setStatusMessages( cs => [...cs, "loaded entries from db"]);
 		console.log('entries = ');
 		console.dir(entries);
@@ -161,6 +189,10 @@ function Export({
 		}
 
 		const doRecording = async () => {
+			if(!entries) {
+				console.error('no entries found');
+				return;
+			}
 			const videoCanvasContext = videoCanvas.getContext('2d');
 			console.log(`got video canvas context`);
 			if(!videoCanvasContext) {
@@ -199,7 +231,12 @@ function Export({
 					console.log(`start draw frame = ${c}`);
 					videoCanvasContext.clearRect( 0, 0, scaledImageWidth, scaledImageHeight);
 					videoCanvasContext.drawImage(scaledCanvas, 0, 0);
-					videoCanvasContext.fillText(`FRAME: ${c}`, 10, 50);
+					if(overlayFrameNumberIsChecked) {
+						videoCanvasContext.fillText(`FRAME: ${c}`, 10, 50);
+					}
+					if(overlayEntryInfoIsChecked) {
+						videoCanvasContext.fillText(`ENTRY INFO: ${c}`, 10, 100);
+					}
 					mediaRecorder.resume();
 					const startTime = Date.now();
 					(canvasStream.getVideoTracks()[0] as CanvasCaptureMediaStreamTrack).requestFrame();
@@ -207,10 +244,13 @@ function Export({
 					(canvasStream.getVideoTracks()[0] as CanvasCaptureMediaStreamTrack).requestFrame();
 					mediaRecorder.pause();
 					console.log(`done drawing frame, time to draw = ${Date.now() - startTime}`);
+					setStatusMessages( cs => [...cs, `generated frame: ${c}/${max-1}, actual frame duration = ${Date.now() - startTime} ms`]);
+					await delay(50);
 				}
 			}
 			await delay(250);
 			mediaRecorder.stop();
+			setVideoIsReady(true);
 			/////const rawAlignedImages:any[] = imagesArray;//await Promise.all(imagePromisesArray)
 			//const alignedImages = rawAlignedImages.slice(0);
 			//const alignedImages = [rawAlignedImages[0], ...rawAlignedImages, rawAlignedImages[rawAlignedImages.length-1]];
@@ -278,12 +318,25 @@ function Export({
 			&& event.target instanceof HTMLInputElement
 			&& event.target.dataset.settingsKeyToModify
 		) {
+			let newValue:string|boolean|number = event.target.value as string;
 			let settingsKeyToModify = event.target.dataset.settingsKeyToModify;
-			let newValue = parseInt(event.target.value as string);
 			console.log('settingsKeyToModify = ', settingsKeyToModify);
-			console.log('value = ', newValue);
 			if(event.target.dataset.settingsKeyToModify === 'exportFrameDuration') {
+				newValue = parseInt(newValue);
+				console.log('value = ', newValue);
 				setFrameDuration(newValue);
+			} else if(event.target.dataset.settingsKeyToModify === 'exportOverlayFrameNumber') {
+				let isChecked:boolean|undefined = event.target.checked;
+				console.log('checked? = ', isChecked);
+				(isChecked === true) ? setOverlayFrameNumberIsChecked(true) : setOverlayFrameNumberIsChecked(false);
+				newValue = isChecked;
+				console.log('value = ', newValue);
+			} else if(event.target.dataset.settingsKeyToModify === 'exportOverlayEntryInfo') {
+				let isChecked:boolean|undefined = event.target.checked;
+				console.log('checked? = ', isChecked);
+				(isChecked === true) ? setOverlayEntryInfoIsChecked(true) : setOverlayEntryInfoIsChecked(false);
+				newValue = isChecked;
+				console.log('value = ', newValue);
 			}
 			clearTimeout(debounceInputTimeout.current);
 			let modifyDbValueHandler = async () => {
@@ -307,12 +360,7 @@ function Export({
     <div>
     	<h2>Export Video Locally (Experimental)</h2>
 			<p>Exports video of progress pictures completely in-browser and local to your device. Currently not very consistent at low frame durations (faster timelapse). Export occurs in real-time.</p>
-			<button
-				type="button"
-				onClick={handleExportVideo}
-			>
-				Export Video
-			</button>
+			<label> Frame Duration (ms):
 			<input
 				ref={frameDurationInputRef}
 				type="number"
@@ -322,6 +370,34 @@ function Export({
 				max={MAX_FRAME_DURATION_MS}
 				min={MIN_FRAME_DURATION_MS}
 			/>
+			</label><br/>
+			<label>Overlay Frame Number?:
+			<input
+				ref={overlayFrameNumberInputRef}
+				type="checkbox"
+				value="true"
+				checked={overlayFrameNumberIsChecked}
+				onChange={handleInputChange}
+				data-settings-key-to-modify="exportOverlayFrameNumber" 
+			/>
+			</label><br/>
+			<label>Overlay Entry Information?:
+			<input
+				ref={overlayEntryInfoInputRef}
+				type="checkbox"
+				value="true"
+				checked={overlayEntryInfoIsChecked}
+				onChange={handleInputChange}
+				data-settings-key-to-modify="exportOverlayEntryInfo" 
+			/>
+			</label><br/>
+			<p>Estimated video duration: { frameDuration && entries ? `${(frameDuration*entries.length/1000)} seconds` : 'N/A'}</p>
+			<button
+				type="button"
+				onClick={handleExportVideo}
+			>
+				Export Video
+			</button>
 			<p>Status:</p>
 			<div style={{
 				border: '1px solid black',
@@ -339,8 +415,22 @@ function Export({
 			</div>
 			<hr/>
 			<h2>Output Video</h2>
-			<video ref={videoElementRef} controls width="350">
+			<video 
+				ref={videoElementRef} 
+				controls 
+				width="350"
+				style={{
+					visibility: (videoIsReady ? 'visible' : 'hidden'),
+					height: (videoIsReady ? 'auto' : '0px')
+				}}
+			> 
 			</video>
+			<hr/>
+			<h2>Export Video Server-side</h2>
+			<p><i>Not yet implemented.</i></p>
+			<hr/>
+			<h2>Upload Interactive Viewer</h2>
+			<p><i>Not yet implemented.</i></p>
 		</div>
   );
 }
