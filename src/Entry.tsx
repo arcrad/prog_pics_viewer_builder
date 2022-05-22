2fe229be181608843979ee82a739aadeba0f9dcc
arcrad <jonathanmaiorana@gmail.com>
2022-05-22 02:55:26 -0400

    added more individual entry validation errors

--- src/Entry.tsx
+++ src/Entry.tsx
@@ -400,13 +400,24 @@ async function verifyPermission(fileHandle: any, readWrite: boolean) {
 		const entryHasAllMarks = entry.marks && Object.keys(entry.marks).length == 3; 
 		const entryDoesntHaveWeight = entry.weight == null || String(entry.weight) == '';
 		const entryDoesntHaveDate = entry.date == null;
-		if( !entryHasAllMarks || entryDoesntHaveWeight || entryDoesntHaveDate) {
+		const entryDoesntHaveImageBlob = entry.imageBlob == null;
+		const entryDoesntHaveAlignedImageBlob = entry.alignedImageBlob == null;
+
+		if( 
+			!entryHasAllMarks 
+			|| entryDoesntHaveWeight 
+			|| entryDoesntHaveDate
+			|| entryDoesntHaveImageBlob
+			|| entryDoesntHaveAlignedImageBlob
+		) {
 			return <div>
 				⚠️  Validation errors exist!
 				<ul>
-					{ entryHasAllMarks ? '' : <li>❌ Entry doesn't have all marks</li>}
+					{ entryDoesntHaveImageBlob ? <li>❌ Entry is is missing image</li> : ''}
 					{ entryDoesntHaveWeight ? <li>❌ Entry is missing weight</li> : ''}
+					{ entryHasAllMarks ? '' : <li>❌ Entry doesn't have all marks</li>}
 					{ entryDoesntHaveDate ? <li>❌ Entry is missing date</li> : ''}
+					{ entryDoesntHaveAlignedImageBlob ? <li>❌ Entry is missing an aligned image</li> : ''}
 				</ul>
 			</div>
 		}
