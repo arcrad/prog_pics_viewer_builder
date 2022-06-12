5036b997f8c3d3290d96f764eed10aa39b34da21
arcrad <jonathanmaiorana@gmail.com>
2022-06-12 03:11:33 -0400

    updated style/layout of ChangeImageComponent and cleaned up code

--- src/ChangeImageComponent.tsx
+++ src/ChangeImageComponent.tsx
@@ -4,42 +4,35 @@ import
 		useState, 
 		useEffect, 
 		useRef, 
-		Dispatch, 
-		SetStateAction, 
-		MouseEvent,
-		ChangeEvent 
 	} from 'react';
 import {
 	useNavigate,
 	useParams,
 } from 'react-router-dom';
 import { useLiveQuery } from 'dexie-react-hooks';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { 
+	faUpload, 
+} from '@fortawesome/free-solid-svg-icons'
 
 import { db, Entry } from './db';
 import { GlobalState } from './App';
-//import './ChangeImageComponent.css';
 
 type ChangeImageComponentAttributes= {
-	//globalState: GlobalState;
-	//setGlobalState: Dispatch<SetStateAction<GlobalState>>;
-//	isModalVisible: boolean;
-//	setIsModalVisible: Dispatch<SetStateAction<boolean>>;
 	closeModalOnLoad: boolean;
 	afterLoadImageFn?: () => void;
 };
 
 function ChangeImageComponent({
-	//globalState, 
-	//setGlobalState,
-//	isModalVisible,
-//	setIsModalVisible,
 	closeModalOnLoad,
 	afterLoadImageFn,
 } : ChangeImageComponentAttributes ) {
 	let [statusMessages, setStatusMessages] = useState<string[]>([]);
 	
 	let modalOverlayRef = useRef<HTMLDivElement>(null);
+	let imageUploadContainerRef = useRef<HTMLDivElement>(null);
 	let imageUploadRef = useRef<HTMLInputElement>(null);
+	let imageUploadFileNameRef = useRef<HTMLSpanElement>(null);
   let loadImageButtonRef = useRef<HTMLButtonElement>(null);
 	let selectImageButtonRef = useRef<HTMLButtonElement>(null);
 	let fileHandleRef = useRef<any>(null);
@@ -48,102 +41,131 @@ function ChangeImageComponent({
 	
 	let { entryId } = useParams();
 	
-	const loadImageHandler = async (event:MouseEvent<HTMLButtonElement>) => {
+	useEffect( () => {
+		if(imageUploadRef && imageUploadRef.current) {
+			imageUploadRef.current.onchange = () => {
+				if(imageUploadRef.current 
+					&& imageUploadRef.current.files
+					&& imageUploadRef.current.files.length > 0) {
+						if(imageUploadFileNameRef.current) {
+		      		imageUploadFileNameRef.current.textContent = imageUploadRef.current.files[0].name;
+							handleImageLoad();
+						}
+    		}
+			};
+		}
+	},[]);
+	
+	const handleImageLoad = async () => {
 		//console.dir(imageUploadRef.current);
 		console.log("handle load image..");
-		setStatusMessages(["adding image start"]);
+		setStatusMessages(["Started loading image..."]);
 		let selectedFile:File;
 		if(imageUploadRef.current && imageUploadRef.current.files) {
 			selectedFile = imageUploadRef.current.files[0];
 			//create temp image to get dimensions
 			let tempImage = new Image();
 			tempImage.onload = () => {
-				setStatusMessages( cs => [...cs, "image file loaded"]);
+				setStatusMessages( cs => [...cs, "Image loaded."]);
 				//generate thumbnail, if needed
 				const thumbMaxDimension = 300;
-				//if(tempImage.naturalWidth > thumbMaxDimension || tempImage.naturalHeight > thumbMaxDimension) {
-					let thumbWidth = tempImage.naturalWidth;
-					let thumbHeight = tempImage.naturalHeight;
-					if( tempImage.naturalWidth > tempImage.naturalHeight) {
-						//landscape
-						thumbWidth = thumbMaxDimension;
-						thumbHeight = tempImage.naturalHeight / (tempImage.naturalWidth / thumbMaxDimension);
-					} else {
-						//square or portrait
-						thumbWidth = tempImage.naturalWidth / (tempImage.naturalHeight / thumbMaxDimension);
-						thumbHeight = thumbMaxDimension;
-					}
-					const thumbCanvas = document.createElement('canvas');
-					thumbCanvas.width = thumbWidth;
-					thumbCanvas.height = thumbHeight;
-					const thumbCanvasContext = thumbCanvas.getContext('2d');
-					if(thumbCanvasContext) {
-						thumbCanvasContext.drawImage(tempImage, 0, 0, thumbWidth, thumbHeight);
-						thumbCanvas.toBlob( (blob) => {
-							if(entryId != null ) {
+				let thumbWidth = tempImage.naturalWidth;
+				let thumbHeight = tempImage.naturalHeight;
+				if( tempImage.naturalWidth > tempImage.naturalHeight) {
+					//landscape
+					thumbWidth = thumbMaxDimension;
+					thumbHeight = tempImage.naturalHeight / (tempImage.naturalWidth / thumbMaxDimension);
+				} else {
+					//square or portrait
+					thumbWidth = tempImage.naturalWidth / (tempImage.naturalHeight / thumbMaxDimension);
+					thumbHeight = thumbMaxDimension;
+				}
+				const thumbCanvas = document.createElement('canvas');
+				thumbCanvas.width = thumbWidth;
+				thumbCanvas.height = thumbHeight;
+				const thumbCanvasContext = thumbCanvas.getContext('2d');
+				if(thumbCanvasContext) {
+					thumbCanvasContext.drawImage(tempImage, 0, 0, thumbWidth, thumbHeight);
+					thumbCanvas.toBlob( (blob) => {
+						if(entryId != null ) {
+						db.entries.update(parseInt(entryId), {
+							thumbImageBlob: blob
+						}).then( () => {
+							//thumb blob saved
+							setStatusMessages( cs => [...cs, "Saved thumbnail data."]);
+							if(entryId != null) {
 							db.entries.update(parseInt(entryId), {
-								thumbImageBlob: blob
+								imageBlob: selectedFile,
+								imageNaturalWidth: tempImage.naturalWidth,
+								imageNaturalHeight: tempImage.naturalHeight
 							}).then( () => {
-								//thumb blob saved
-								setStatusMessages( cs => [...cs, "saved thumbnail data"]);
-								if(entryId != null) {
-								db.entries.update(parseInt(entryId), {
-									imageBlob: selectedFile,
-									imageNaturalWidth: tempImage.naturalWidth,
-									imageNaturalHeight: tempImage.naturalHeight
-								}).then( () => {
-									setStatusMessages( cs => [...cs, "saved full-res image data"]);
-									if(closeModalOnLoad) {
-									//	setIsModalVisible(false);
-										navigate('/entry');
-										setStatusMessages([]);
-									}
-									if(afterLoadImageFn) {
-										afterLoadImageFn();
-									}
-								});
+								setStatusMessages( cs => [...cs, "Saved full resolution image data."]);
+								//make upload button success color
+								if(imageUploadContainerRef.current) {
+									imageUploadContainerRef.current.classList.add('is-success');
+								}
+								if(closeModalOnLoad) {
+									navigate('/entry');
+									setStatusMessages([]);
+								}
+								if(afterLoadImageFn) {
+									afterLoadImageFn();
 								}
 							});
 							}
 						});
-					}
-				//} else {
-					//use original image as thumbnail
-				//}
+						}
+					});
+				}
 			}
 			tempImage.src = URL.createObjectURL(selectedFile);
 			//console.log('selected file objectURL=');
 			//console.dir(URL.createObjectURL(selectedFile));
 		}
 	};
-	
-	const selectImageHandler = async (event:MouseEvent<HTMLButtonElement>) => {
-		  const [fileHandle] = await window.showOpenFilePicker();
-			console.log('fileHandle = ');
-			console.dir(fileHandle);
-			fileHandleRef.current = fileHandle;
-	};
-
 	return (
-			<>
-				<p>Updating entry with id = { entryId }.</p>
-				<p>Messages:</p>
-				<ul>
-				{
-					statusMessages?.map( (message, index) => {
-						return <li key={index}>{message}</li>
-					})
-				}
-				</ul>
-				{/*<button ref={selectImageButtonRef} type="button" onClick={selectImageHandler}>
-					Select Image
-				</button>*/}
-				<input ref={imageUploadRef} type="file"></input>
-				<button ref={loadImageButtonRef} type="button" onClick={loadImageHandler}>
-					Load Image
-				</button>
-				{/*<button type="button" onClick={ () => setIsModalVisible(false) }>Cancel</button>*/}
-			</>
+		<>
+			<div className="columns">
+				<div className="column is-flex is-justify-content-center">
+					<div 
+						ref={imageUploadContainerRef}
+						className="file is-boxed has-name"
+					>
+						<label className="file-label">
+							<input 
+								ref={imageUploadRef} 
+								type="file"
+								className="file-input"
+							/>
+							<span className="file-cta">
+								<FontAwesomeIcon icon={faUpload}/>
+								<span className="file-label">
+									Choose an image...
+								</span>
+							</span>
+							<span ref={imageUploadFileNameRef} className="file-name"></span>
+						</label>
+					</div>
+				</div>
+			</div>
+			<div className="columns">
+				<div className="column is-cenetered is-12">
+					<div className="field">
+						<label className="label">Image Load Status</label>
+						<div className="control">
+							<textarea
+								className="textarea"
+								value={
+									statusMessages?.reduce( (accumulator, message) => {
+										return accumulator + message + "\n";
+									}, "")
+								}
+							></textarea>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
   );
 }
 
