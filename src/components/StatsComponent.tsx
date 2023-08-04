cbfbe801d20443bdf58987602b645bdfb2a05daf
arcrad <jonathanmaiorana@gmail.com>
2023-08-03 22:42:50 -0400

    continued with skeleton for stats component
    installed chartjs dependencies
    implemented test line chart

--- src/components/StatsComponent.tsx
+++ src/components/StatsComponent.tsx
@@ -6,17 +6,41 @@ import
 		useEffect
 	}
 from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 type StatsComponentProps = {
+	data: any;
 	expanded: boolean;
 };
 
 function StatsComponent({
+	data,
 	expanded
 }: StatsComponentProps) {
 	return (
 		<div>
 			<p>stats {expanded}</p>
+			<Line data={data} />
 		</div>
 	);
 }
